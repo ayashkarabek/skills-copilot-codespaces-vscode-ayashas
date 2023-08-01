7c8f6204209fc6db0519475d09c9acaff85681ae
ayashkarabek <110608394+ayashkarabek@users.noreply.github.com>
2023-08-01 14:29:23 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,15 @@
+// Create web server
+
+// Import modules
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator');
+
+// Import models
+const Comment = require('../models/Comment');
+const User = require('../models/User');
+const Post = require('../models/Post');
+
+// Import middleware
+const auth = require('../middleware/auth');
+
